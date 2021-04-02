fdacba91f2dab78cc78c3852412991cbd0ba5a41
Leo <zhanzhikai2015@gmail.com>
2021-04-02 17:18:55 -0400

    updated searchJobApi

--- routes/searchJobAPI.js
+++ routes/searchJobAPI.js
@@ -4,17 +4,25 @@ const superagent = require('superagent');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    // to be fixed
+    console.log( `body here: ${req.query}`);
+
     let locations = req.query.locations;
-    let keywords = req.query.keywords;
+    let skills = req.query.skills;
+   
+
+    let q = skills.join(", ");
+    let l = locations.join(", ");
+   
 
     superagent.get('http://api.indeed.com/ads/apisearch')
         .query({
             publisher: '7778623931867371',
             v: '2',
             format: 'json',
-            q: 'C++, Java, Python, html, PHP, C, PERL,Communication, operation, management',
-            l: 'Waterloo',
+            // q: 'C++, Java, Python, html, PHP, C, PERL,Communication, operation, management',
+            // l: 'Waterloo',
+            q: q,
+            l: l,
             radius: 50,
             limit: 3,
             sort: 'date',
