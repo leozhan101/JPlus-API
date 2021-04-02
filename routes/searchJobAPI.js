fdf4184f025b714fbdcf5a4cf5ae87c36020f565
Leo <zhanzhikai2015@gmail.com>
2021-04-02 15:07:12 -0400

    searchJobAPI in progress

--- routes/searchJobAPI.js
+++ routes/searchJobAPI.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const superagent = require('superagent');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('searchJob API page');
+    // to be fixed
+    let locations = req.query.locations;
+    let keywords = req.query.keywords;
+
+    superagent.get('http://api.indeed.com/ads/apisearch')
+        .query({
+            publisher: '7778623931867371',
+            v: '2',
+            format: 'json',
+            q: 'C++, Java, Python, html, PHP, C, PERL,Communication, operation, management',
+            l: 'Waterloo',
+            radius: 50,
+            limit: 3,
+            sort: 'date',
+            highlight: 1,
+            filter: 1,
+            latlong: 1,
+            co: 'canada'.toLowerCase(),
+            userip: '',
+            useragent: ''
+        })
+        .end((err, ans) => {
+            if (err) { return console.log(err); }
+            console.log(ans.body);
+            res.send(ans.body);
+            res.send(ans.body.jobtitle);
+        });
 });
 
 module.exports = router;
