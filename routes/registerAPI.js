2234c2f8bc2c87799ff7dde6f2ee37dba3b42361
Leo <zhanzhikai2015@gmail.com>
2021-04-02 14:31:26 -0400

    updated registerAPI

--- routes/registerAPI.js
+++ routes/registerAPI.js
@@ -4,26 +4,16 @@ var db = require('../dbConfig');
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
     let username = req.body.username;
-    let firstname = req.body.firstName;
-    let lastname = req.body.lastName;
     let password = req.body.password;
-
-    console.log("body: ", req.body);
-
-    console.log("query: ", req.query);
-
-    console.log("param: ", req.params);
-
-    // console.log("req: ", req);
-    console.log(username);
-    console.log(firstName);
 
     myObj = {username: username, firstname: firstname, lastname: lastname, password: password}
 
     db.insert(myObj)
 
-    res.send('Just Inserted');
+    // res.send('Just Inserted');
 });
 
 module.exports = router;
